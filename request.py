fc35d09ade045d704f647c9298cc176408147dc5
prabhatpokharel <pokharelprabhat@gmail.com>
2022-09-08 15:11:35 +0545

    Update request.py

--- request.py
+++ request.py
@@ -1,8 +1,19 @@
-from django.http import HttpResponse
-
-def index(request):
-    value = request.GET.get("value")
-    response = HttpResponse("")
-    response["Set-Cookie"] = value  # Noncompliant
-    response.set_cookie("sessionid", value)  # Noncompliant
-    return response
+import tweepy
+ 
+# API keyws that yous saved earlier
+api_key = "Abbwejkbsjgb"
+api_secrets = "237hbsdfibnr3277bfwhefoghaohf"
+access_token = " nefjhn2q3y7723r5i8"
+access_secret = "wefnpiu237bngwniuwe"
+ 
+# Authenticate to Twitter
+auth = tweepy.OAuthHandler(api_key,api_secrets)
+auth.set_access_token(access_token,access_secret)
+ 
+api = tweepy.API(auth)
+ 
+try:
+    api.verify_credentials()
+    print('Successful Authentication')
+except:
+    print('Failed authentication')
